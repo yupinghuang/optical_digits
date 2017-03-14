67fbedb2ce17272f7b4989c12a77ee4c5c22bf69
Yuping Huang <yupinghyper@gmail.com>
2017-03-13 19:04:51 -0500

    maxent fixed?

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from DataSet import DataSet
 import Classfiers
-from FeatureExtractors import AllGridExtractor, MaxEntFeatureExtractor, DecisionTreeFeatureExtractor
+from FeatureExtractors import *
 import random
 
 if __name__=='__main__':
@@ -20,13 +20,17 @@ if __name__=='__main__':
     MIRAClassfier.train(trainingSet)
     MIRAClassfier.test(holdoutSet)
     '''
-    maxEntClassifier = Classfiers.MaxEnt(featureExtractor=MaxEntFeatureExtractor())
-    maxEntClassifier.train(trainingSet)
+    maxEntClassifier = Classfiers.MaxEnt(featureExtractor=MaxEntBinaryFeatureExtractor())
+    maxEntClassifier.train(trainingSet, iterations=15)
+    maxEntClassifier.test(trainingSet)
+    '''
     print 'TEST ON TRAINING SET!!!'
     maxEntClassifier.test(trainingSet)
     '''
+
+    '''
     dt = Classfiers.DecisionTree(featureExtractor=DecisionTreeFeatureExtractor())
     dt.train(trainingSet)
-    dt.test(testSet)
+    dt.test(holdoutSet)
 
     '''
